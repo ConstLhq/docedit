e3a56278749cefb5438d4c8df65c1fa8852d3620
ConstLhq <const_lhq@126.com>
2017-05-03 16:29:51 +0800

    集成mapnik之前

--- app/controllers/uploadController.js
+++ app/controllers/uploadController.js
@@ -29,293 +29,305 @@ var options = {
     req: "parse",
     txt: "",
     type: "",
-    time: "",
+    t: "",
   }
 };
 
 //multer有single()中的名称必须是表单上传字段的name名称。
-var docSaveCb = function(err, newdoc, results, req, res) {
+var docSaveCb = function(err, newdoc, newevents, req, res) {
+  if (err) {
+    console.log(err)
+  }
+  console.log("文件内容存入了数据库！")
+
+  //填充Event数据集
+  Event.insertMany(newevents.map(function(event) {
+    event.fromdoc = newdoc._id
+    return event
+  }), (err, _es) => {
+    var i = 0; //段落搜索索引
     if (err) {
       console.log(err)
     }
-    console.log("文件内容存入了数据库！")
-
-    //填充Event数据集
-    Event.insertMany(results.All.EVENT.map(function(result) {
-          return {
-            fromdoc: newdoc._id,
-            content: result._,
-            location: result._key.locname,
-            lat: result._key.lat,
-            lng: result._key.lng,
-            time: "timevalue" in result._key ? result._key.timevalue : "NONE"
-          }
-        }), (err, _es) => {
-
-          var i = 0; //段落搜索索引
+    _es.forEach(function(e,_i) {
+     
+      var flag = -1
+      while (i < newdoc.paragraph.length) {
+        flag = newdoc.paragraph[i].indexOf(e.content)
+        if (flag != -1) {
+          e.fromparagraph = i
+          e.offsetS = flag
+          e.offsetE = flag + e.content.length
+          e.save((err, d) => {
+            if (err) {
+              console.log(err)
+            }
+          })
+          return
+        } else {i++}
+      }
+      if (flag == -1) { //找死没有
+        e.fromparagraph = -1
+        e.offsetS = -1
+        e.offsetE = -1
+        e.save((err, d) => {
           if (err) {
             console.log(err)
           }
-          console.log(_es)
-          _es.forEach(function(e) {
-            do {
-              console.log(i)
-              console.log(newdoc.paragraph.length)
-              var flag = newdoc.paragraph[i].indexOf(e.content)
-              if (flag != -1) {
-                e.fromparagraph = i
-                e.offsetS = flag
-                e.offsetE = flag + e.content.length
-                e.save((err, d) => {
+        })
+
+      }
+    })
+  })
+
+
+  User.findById(req.session.user._id, function(err, user) {
+    if (err) {
+      console.log(err)
+    }
+    if (user.docs.indexOf(newdoc._id) == -1) {
+     
+      var gr = tu = 0;
+      user.docs.push(newdoc._id)
+      user.group.forEach(function(grp, _gr) {
+
+          
+
+        if (grp.groupName == req.body.docGroup) {
+          grp.groupFile.push(newdoc._id);
+          tu = grp.groupFile.length - 1;
+          gr = _gr;
+        }
+      })
+      user.save(function(err, user) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.json([gr, tu])
+        }
+      })
+    }
+  })
+}
+exports.dataInput = function(req, res) {
+  upload(req, res, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    //文件信息在req.file或者req.files中显示。
+    var file = req.file
+    console.log(file.path)
+    options.form.type = req.body.docType
+    options.form.t = new Date(req.body.docDate).toISOString()
+
+    //添加全文解析和切分
+    function parseAndCut(filedata) {
+      console.log("in parse")
+        //拆分解析
+      var paragraphs = filedata.split(/\n/)
+      paragraphs = paragraphs.filter(function(s) {
+        return s != ""
+      })
+
+      var doc = new Mydoc({
+        filePath: path.join(__dirname, "../../", file.path),
+        type: req.body.docType,
+        referenceTime: new Date(req.body.docDate).toISOString(),
+        originalName: file.originalname,
+        owner: req.session.user._id,
+        sequence: req.body.sequence,
+        rawContent: filedata,
+        paragraph: paragraphs,
+        groupName: req.body.docGroup,
+      })
+
+
+      var counter = 0
+      var contextarry = [null, null]
+      var newevents = []
+      paragraphs.forEach(function(s, p_index) {
+        console.log(s)
+        var events = new Cut_Events(contextarry);
+
+        options.form.txt = s.replace(/\s/g, '')
+        request(options, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var parsedXML = entities.decodeXML(body)
+            console.log( parsedXML)
+            var i = parsedXML.indexOf("<content>");
+            var e = parsedXML.indexOf("</content>");
+            parsedXML = parsedXML.substring(i + 9, e)
+            var data = events._cutScene(parsedXML.replace(/\n/g, ""))
+            contextarry = events.getBgContext()
+            parser.parseString("<All>" + data + "</All>", function(err, results) {
+              if (err) {
+                console.log(err)
+              } else {
+                newevents = newevents.concat(results.All.EVENT.map(function(event) {
+                  if (event._key != undefined) {
+                    return {
+                      content: event._,
+                      location: event._key.locname,
+                      lat: event._key.lat ,
+                      lng: event._key.lng ,
+                      time: "timevalue" in event._key ? event._key.timevalue : null
+                    }
+                  } 
+                }))
+              }
+              counter += 1
+                //所有回调函数执行完成
+              if (counter == paragraphs.length) {
+                doc.save(docSaveCb(err, doc, newevents, req, res))
+              }
+            })
+          }
+        })
+      })
+    }
+
+    switch (file.originalname.split(".")[1]) {
+      case "txt":
+        fs.readFile(file.path, {
+          encoding: 'utf-8'
+        }, (err, filedata) => {
+          if (err) {
+            console(err)
+          } else {
+            parseAndCut(filedata)
+          }
+        })
+        break;
+      case "docx":
+        mammoth.extractRawText({
+          path: file.path
+        }).then(function(result_raw) {
+          parseAndCut(result_raw.value)
+        });
+        break;
+    }
+  })
+}
+
+exports.devide = function(req, res) {
+
+  Mydoc.findById(req.body.docid, function(err, doc) {
+    if (err) {
+      console.log(err)
+    } else {
+
+      var oldhtml = doc.htmlContent
+      var s = oldhtml.indexOf(req.body.str)
+      var newparagraph = new Array
+
+      for (var i = 0; i < doc.paragraph.length; i++) {
+        var str = req.body.str
+        var s = doc.paragraph.indexOf(str)
+        if (s == -1) {
+          newparagraph.push(doc.paragraph[i])
+        } else {
+          newparagraph.push(doc.paragraph[i].substring(0, s + str.length))
+          newparagraph.push(doc.paragraph[i].substr(s + str.length))
+          break;
+        }
+
+      }
+
+      doc.paragraph = newparagraph
+      doc.save(function(err, doc) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("doc updated")
+        }
+      })
+      res.send({
+        paragraph: doc.paragraph
+      })
+    }
+  })
+}
+
+exports.reparse = function(req, res) {
+
+  Mydoc.findById(req.body.docid, function(err, doc) {
+    if (err) {
+      console.log(err)
+    } else {
+      var oldhtml = doc.htmlContent
+        //拆分解析
+      var paragraphs = oldhtml.split(/<\/p><p.*?>/)
+      paragraphs = paragraphs.map(function(s) {
+        return s.replace(/<\/?.*?>/, '')
+      })
+      paragraphs = paragraphs.filter(function(s) {
+        return s != ""
+      })
+      var counter = 0
+      var newevents = []
+      paragraphs.forEach(function(s) {
+        var events = new Cut_Events();
+        options.form.txt = s.replace(/\s/g, '')
+        request(options, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var parsedXML = entities.decodeXML(body)
+            var i = parsedXML.indexOf("<content>");
+            var e = parsedXML.indexOf("</content>");
+            parsedXML = parsedXML.substring(i + 9, e)
+            var data = events._cutScene(parsedXML.replace(/\n/g, ""))
+            parser.parseString("<All>" + data + "</All>", function(err, results) {
+              if (err) {
+                console.log(err)
+              } else {
+                newevents = newevents.concat(results.All.EVENT.map(function(event) {
+                  if (event._key != undefined) {
+                    return {
+                      CONTENT: event._,
+                      LOC: event._key.locname || "不明",
+                      LAT: event._key.lat || "不明",
+                      LNG: event._key.lng || "不明",
+                      TIME: "timevalue" in event._key ? event._key.timevalue : "不明"
+                    }
+                  } else {
+                    return {
+                      CONTENT: event._,
+                      LOC: "不明",
+                      LAT: "不明",
+                      LNG: "不明",
+                      TIME: "不明"
+                    }
+                  }
+                }))
+              }
+              counter += 1
+              if (counter == paragraphs.length) {
+                //所有回调函数执行完成
+                doc.Events = newevents
+                doc.save(function(err, d) {
                   if (err) {
                     console.log(err)
                   }
                 })
-              } else {
-                i++
+                res.json(newevents)
               }
-            } while (i < newdoc.paragraph.length-1  && flag == -1)
-          })
+            })
+          }
         })
+      })
+    }
+  })
+}
 
-
-          User.findById(req.session.user._id, function(err, user) {
-            if (err) {
-              console.log(err)
-            }
-            if (user.docs.indexOf(newdoc._id) == -1) {
-              var gr = tu = 0;
-              user.docs.push(newdoc._id)
-              user.group.forEach(function(grp, _gr) {
-                if (grp.groupName == req.body.docGroup) {
-                  grp.groupFile.push(newdoc._id);
-                  tu = grp.groupFile.length - 1;
-                  gr = _gr;
-                }
-              })
-              user.save(function(err, user) {
-                if (err) {
-                  console.log(err)
-                } else {
-                  res.json([gr, tu])
-                }
-              })
-            }
-          })
-
-
-        }
-        exports.dataInput = function(req, res) {
-          upload(req, res, function(err) {
-            if (err) {
-              return console.log(err);
-            }
-            //文件信息在req.file或者req.files中显示。
-            var file = req.file
-            console.log(file.path)
-            options.form.type = req.body.docType
-            options.form.t = new Date(req.body.docDate).toISOString()
-
-            //添加全文解析和切分
-            function parseAndCut(filedata) {
-              var events = new Cut_Events();
-              options.form.txt = filedata.replace(/\s/g, '')
-              request(options, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  var parsedXML = entities.decodeXML(body)
-
-                  console.log(parsedXML)
-                  var i = parsedXML.indexOf("<content>");
-                  var e = parsedXML.indexOf("</content>");
-
-                  parsedXML = parsedXML.substring(i + 9, e)
-
-                  var data = events._cutScene(parsedXML.replace(/\n/g, ""))
-
-                  var raw = parsedXML.replace(/(<\/?.*?>|\n)/g, '');
-
-                  var html = '<p class= "plaintext">' + raw + '</p>'
-                  parser.parseString("<All>" + data + "</All>", function(err, results) {
-                    if (err) {
-                      console.log(err)
-                    }
-
-
-                    var doc = new Mydoc({
-                        filePath: path.join(__dirname, "../../", file.path),
-                        type: req.body.docType,
-                        referenceTime: new Date(req.body.docDate).toISOString(),
-                        originalName: file.originalname,
-                        owner: req.session.user._id,
-                        sequence: req.body.sequence,
-                        rawContent: raw,
-                        Sentences: data.replace(/<\/EVENT>/g, "$&||").split("||"),
-                        paragraph: filedata.split(/\n/),
-                        groupName: req.body.docGroup,
-                      })
-                    doc.save(docSaveCb(err, doc, results, req, res))
-                  })
-                }
-              })
-            }
-
-            switch (file.originalname.split(".")[1]) {
-              case "txt":
-                fs.readFile(file.path, {
-                  encoding: 'utf-8'
-                }, (err, filedata) => {
-                  if (err) {
-                    console(err)
-                  } else {
-                    parseAndCut(filedata)
-                  }
-                })
-                break;
-              case "docx":
-                mammoth.extractRawText({
-                  path: file.path
-                }).then(function(result_raw) {
-                  parseAndCut(result_raw.value)
-                });
-                break;
-            }
-          })
-        }
-
-        exports.devide = function(req, res) {
-
-
-          Mydoc.findById(req.body.docid, function(err, doc) {
-            if (err) {
-              console.log(err)
-            } else {
-
-              var oldhtml = doc.htmlContent
-              var s = oldhtml.indexOf(req.body.str)
-
-              var e = s + req.body.str.length
-
-              var newhtml = oldhtml.substring(0, s) + "</p><p class =\"plaintext\">" + oldhtml.substring(s, e) + '</p><p class =\"plaintext\">' + oldhtml.substr(e)
-
-              doc.htmlContent = newhtml
-              doc.save(function(err, doc) {
-                if (err) {
-                  console.log(err)
-                } else {
-                  console.log("doc updated")
-                }
-              })
-              res.send(newhtml)
-
-            }
-          })
-        }
-
-        exports.reparse = function(req, res) {
-
-          Mydoc.findById(req.body.docid, function(err, doc) {
-            if (err) {
-              console.log(err)
-            } else {
-
-              var oldhtml = doc.htmlContent
-
-              //拆分解析
-              var paragraphs = oldhtml.split(/<\/p><p.*?>/)
-
-
-              paragraphs = paragraphs.map(function(s) {
-                return s.replace(/<\/?.*?>/, '')
-              })
-              paragraphs = paragraphs.filter(function(s) {
-                return s != ""
-              })
-            
-              var counter = 0
-
-              var newevents = []
-
-              paragraphs.forEach(function(s) {
-
-                var events = new Cut_Events();
-
-                options.form.txt = s.replace(/\s/g, '')
-
-                request(options, function(error, response, body) {
-
-                  if (!error && response.statusCode == 200) {
-
-                    var parsedXML = entities.decodeXML(body)
-
-                    var i = parsedXML.indexOf("<content>");
-                    var e = parsedXML.indexOf("</content>");
-
-                    parsedXML = parsedXML.substring(i + 9, e)
-
-                    var data = events._cutScene(parsedXML.replace(/\n/g, ""))
-
-                    parser.parseString("<All>" + data + "</All>", function(err, results) {
-                      if (err) {
-                        console.log(err)
-                      } else {
-
-                        newevents = newevents.concat(results.All.EVENT.map(function(event) {
-
-                          if (event._key != undefined) {
-                            return {
-
-                              CONTENT: event._,
-                              LOC: event._key.locname || "不明",
-                              LAT: event._key.lat || "不明",
-                              LNG: event._key.lng || "不明",
-                              TIME: "timevalue" in event._key ? event._key.timevalue : "不明"
-                            }
-                          } else {
-                            return {
-                              CONTENT: event._,
-                              LOC: "不明",
-                              LAT: "不明",
-                              LNG: "不明",
-                              TIME: "不明"
-                            }
-
-                          }
-                        }))
-
-                      }
-
-                      counter += 1
-                      if (counter == paragraphs.length) {
-
-                        //所有回调函数执行完成
-                        doc.Events = newevents
-                        doc.save(function(err, d) {
-                          if (err) {
-                            console.log(err)
-                          }
-                        })
-
-                        res.json(newevents)
-
-                      }
-                    })
-                  }
-                })
-              })
-            }
-          })
-        }
-
-        exports.downloadFile = function(req, res) {
-          console.log(req.params.docid)
-            //
-          Mydoc.findById(req.params.docid, (err, doc) => {
-            if (err) {
-              res.json({
-                error: "download failed"
-              })
-            } else {
-              res.download(doc.filePath, doc.originalName)
-            }
-          })
-        }
+exports.downloadFile = function(req, res) {
+  console.log(req.params.docid)
+    //
+  Mydoc.findById(req.params.docid, (err, doc) => {
+    if (err) {
+      res.json({
+        error: "download failed"
+      })
+    } else {
+      res.download(doc.filePath, doc.originalName)
+    }
+  })
+}
--- app/modules/event.js
+++ app/modules/event.js
@@ -1,5 +1,5 @@
-const TimeContext= require("./timeContext"),
-	  PlaceContext=require("./placeContext");
+const TimeContext = require("./timeContext"),
+	PlaceContext = require("./placeContext");
 
 function EventObj(eo) {
 	if (eo) {
@@ -17,13 +17,13 @@ function EventObj(eo) {
 }
 
 function Event(contextarry) {
-	this.bg_tc= contextarry[0]? new TimeContext(contextarry[0]):null
-	this.bg_pc= contextarry[1]? new PlaceContext(contextarry[1]):null
+	this.bg_tc = contextarry[0] ? new TimeContext(contextarry[0]) : null
+	this.bg_pc = contextarry[1] ? new PlaceContext(contextarry[1]) : null
 }
 
 Event.prototype = {
 	_cutScene: function(data) {
-
+		
 		/*
 		算法思路：
 		按照时间和句子标点切割句子切割成[[s1,s2,s3,s4...],[    ],[    ],[   ],[  ]]
@@ -32,46 +32,56 @@ Event.prototype = {
 		//- todo 分段支持
 		*/
 
-
-		var str = data.replace(/(。|\?|？|\.{6}|\.{3}])/g, "$&<break>")
-		var rs = data.replace(/(。|\?|？|\.{6}|\.{3}])/g, "$&<break>").split("<break>")
-		var pc = null,
+		var r = data
+			.replace(/(。|\?|？|\.{6}|\.{3})/g, "$&<break>")
+			.replace(/：/g, ":") //统一中文冒号换成英文冒号
+			.replace(/:(?=\d*?@LOC)/g, "：") //uuid 中的英文冒号换成中文冒号
+			.replace(/:/g, "$&<break>") //剩下的英文冒号后添加分隔符<break>
+			.replace(/：(?=\d*?@LOC)/g, ":"), //还原uuid的冒号
+			rs = r.split("<break>"),
+			pc = null,
 			tc = null,
 			TAG_LOC_BEGIN = "<Location",
 			TAG_TIME_BEGIN = "<time",
 			TAG_TIME_END = "</time>",
 			s,
 			e,
+			pu,
+			pe,
 			objs,
 			splits = [],
 			times = [],
-			str
-			//对应原来java代码替换uuid中的冒号为中文冒号的代码
-		rs = rs.map(function(_str) {
-			//统一中文冒号换成英文冒号
-			_str = _str.replace(/：/g, ":")
-				//uuid 中的英文冒号换成中文冒号
-			_str = _str.replace(/:(?=\d*?@LOC)/g, "：")
-				//剩下的英文冒号后添加分隔符|||
-			_str = _str.replace(/:/g, "$&|||")
-				//还原uuid的冒号
-			_str = _str.replace(/：(?=\d*?@LOC)/g, ":")
-			return _str
-		})
+			str;
+
 		for (var i = 0; i < rs.length; i++) {
 			var str = rs[i]
-			var subs = str.split("|||");
-			if (subs.length == 2) {
-
-				rs = rs.slice(0, i - 1).concat(subs).concat(rs.slice(i + 1, rs.length))
-				str = subs[0];
-			}
 			s = str.indexOf(TAG_TIME_BEGIN); //<time
-			str = str.replace(/，/g, ',').replace(/,/g, "，<break>");
+			ps = str.indexOf(TAG_LOC_BEGIN); //<Location
+			str = str.replace(/(，|,)/g, '$&<break>');
 			//逗号切割
 			var toks = str.split("<break>")
-			//该自然句不包含时间
+				//该自然句不包含时间
 			if (s == -1) {
+			console.log("in")
+				//如果也不包含地址，将与前后文合并
+				//首句向后合并，其余合并到前文
+				if (ps == -1) {
+					if(i>0  ){
+						//中间句与前句合并
+						rs=rs.slice(0, i - 1).concat([rs[i-1]+rs[i]]).concat(rs.slice(i + 1, rs.length))
+						i--
+					}else if(i==0 && rs.length>1){
+						//首句与后面合并
+						console.log(rs[0])
+						console.log(rs[1])
+						rs=[rs[0]+rs[1]].concat(rs.slice(2,rs.length))
+						i=-1
+					}else if(i==0 && rs.length==1){ //合成了一句话，而且啥也没有
+						break
+					}
+				}
+				else{ //包含地址
+
 				// 该句使用背景时间
 				if (splits.length == 0) {
 					var vs = [];
@@ -82,42 +92,48 @@ Event.prototype = {
 					//splits=[[],[],[]...]
 					splits[splits.length - 1].push(t)
 				})
+					
+				}
 			} else { //包含时间
+
+				// console.log("baohanshijian")
+
 				var vs = []
 				splits.push(vs);
-				var addNew = false; 
-				toks.forEach(function(t,index,array) {
+				var addNew = false;
+				for(var index = 0 ;index < toks.length;index++) {
+					t=toks[index]
 					s = t.indexOf(TAG_TIME_BEGIN)
 					if (s != -1) { //有时间
-						e=t.indexOf(TAG_TIME_END,s + TAG_TIME_BEGIN.length)
-						//时间后面一个逗号
-						if (e != -1 && e + TAG_TIME_END.length == t.length - 1 && t[t.length-1]=="，")
-						{
-							// splits.get(splits.size() - 1).add(t);
+						e = t.indexOf(TAG_TIME_END, s + TAG_TIME_BEGIN.length)
+							//时间后面一个逗号
+						if (e != -1 && e + TAG_TIME_END.length == t.length - 1 && new RegExp(/(,|，)/).test(t[t.length - 1])) {
 							// 不是最后一句，此句应与后续句子合为一句
-							if (index + 1 < array.length)
-							{
-								toks[index + 1] = toks[index] + toks[index + 1];
+							if (index + 1 < toks.length) {
+								toks=toks.slice(0, index).concat([toks[index]+toks[index+1]]).concat(toks.slice(index + 2, toks.length))
+								index--	
+		
 							}
-						}else{ //正常情况下，为每个时间开辟新的空间 splits=[[t1,tok1,tok2,...],[t2],[t3]]
+						} else { //正常情况下，为每个时间开辟新的空间 splits=[[t1,tok1,tok2,...],[t2],[t3]]
 
-						tc = new TimeContext(t.substring(s));
-						times.push(tc);
-						if (addNew == false) {
-							splits[splits.length - 1].push(t);
-							addNew = true;
-						} else {
-							//添加新的 [[]]
-						var vs = [];
-						splits.push(vs);
-						splits[splits.length - 1].push(t);
+							tc = new TimeContext(t.substring(s));
+							times.push(tc);
+							if (addNew == false) {
+								splits[splits.length - 1].push(t);
+								addNew = true;
+							} else {
+								//添加新的 [[]]
+								var vs = [];
+								splits.push(vs);
+								splits[splits.length - 1].push(t);
+							}
 						}
-					}
-					} else
+					} else //toks[i]中没有时间，加到上句后面
 						splits[splits.length - 1].push(t);
-				})
+				}
 			}
 		}
+
 		var nobjs = [];
 		var local = "";
 		// console.log(splits)
@@ -125,26 +141,26 @@ Event.prototype = {
 		for (var n = 0; n < splits.length; n++) {
 			local = "";
 			for (var m = 0; m < splits[n].length; m++) {
-				s = splits[n][m].indexOf(TAG_LOC_BEGIN);
+				ps = splits[n][m].indexOf(TAG_LOC_BEGIN);
 				//不包含地址的句子合并
-				if (s == -1) { 
+				if (ps == -1) {
 					local += splits[n][m];
 				}
 				//包含地点的句子判断包含关系
 				else {
 					//初试循环时，pc==null
-					if (pc != null) { 
+					if (pc != null) {
 
-						var npc = new PlaceContext(splits[n][m].substr(s));
+						var npc = new PlaceContext(splits[n][m].substr(ps));
 						// console.log(npc)
 						// 检查是否语义变化
 						// 暂时比较PlaceContext的值
 						if (!npc.isEqual(pc)) {
 
 							var sr = npc.spatialRelation(pc)
-							// console.log(sr.toUpperCase())
-							if (sr.toUpperCase() != "SR_CONTAIN"){
-								
+								// console.log(sr.toUpperCase())
+							if (sr.toUpperCase() != "SR_CONTAIN") {
+
 								if (m > 0) {
 									// 位置发生变化， 进行切割
 									var eo = new EventObj();
@@ -158,14 +174,13 @@ Event.prototype = {
 								}
 								local = splits[n][m];
 								pc = npc;
-							} else
+							} else //位置包含
 								local += splits[n][m];
-						} else
+						} else //位置相同
 							local += splits[n][m];
-					} else {
-						
-						//为pc赋初值
-						pc = new PlaceContext(splits[n][m].substr(s));
+					} else {//无位置信息
+
+						pc = new PlaceContext(splits[n][m].substr(ps));
 						local += splits[n][m];
 					}
 				}
@@ -174,7 +189,7 @@ Event.prototype = {
 				var eo = new EventObj();
 				eo.content = local;
 				//此时 如果仍然 pc==null，说明句子中没有地址则使用 bg_pc
-				eo.pc = pc?pc:this.bg_pc;
+				eo.pc = pc ? pc : this.bg_pc;
 				if (n < times.length)
 					eo.tc = times[n];
 				else
@@ -183,29 +198,31 @@ Event.prototype = {
 			}
 		}
 
-		this.last_tc=times[times.length - 1]
-		this.last_pc=pc
-		
+		this.last_tc = times[times.length - 1]
+		this.last_pc = pc
+
 		var out = "";
 		for (var n = 0; n < nobjs.length; n++) {
-			
+
 			var eo = nobjs[n];
 			if (eo.tc != null) {
 				if (eo.pc != null)
-					out += '<EVENT timevalue="' + eo.tc.value +  '" lng="' + eo.pc.lng + '" lat="' + eo.pc.lat +  '" locname="'+ eo.pc.origin+'" >' + eo.content.replace(/<\/?.+?>/g,'') + '</EVENT>';
+					out += '<EVENT timevalue="' + eo.tc.value + '" lng="' + eo.pc.lng + '" lat="' + eo.pc.lat + '" locname="' + eo.pc.origin + '" >' + eo.content.replace(/<\/?.+?>/g, '') + '</EVENT>';
 				else
-					out += '<EVENT timevalue="' + eo.tc.value + '" >' + eo.content.replace(/<\/?.+?>/g,'') + '</EVENT>';
+					out += '<EVENT timevalue="' + eo.tc.value + '" >' + eo.content.replace(/<\/?.+?>/g, '') + '</EVENT>';
 			} else if (eo.pc != null)
-				out += '<EVENT lng="' + eo.pc.lng + '" lat="' + eo.pc.lat +  '" locname="'+ eo.pc.origin+'" >' + eo.content.replace(/<\/?.+?>/g,'') + '</EVENT>';
+				out += '<EVENT lng="' + eo.pc.lng + '" lat="' + eo.pc.lat + '" locname="' + eo.pc.origin + '" >' + eo.content.replace(/<\/?.+?>/g, '') + '</EVENT>';
 			else
-				out += '<EVENT  >' + eo.content.replace(/<\/?.+?>/g,'') + '</EVENT>';			
+				out += '<EVENT  >' + eo.content.replace(/<\/?.+?>/g, '') + '</EVENT>';
 		}
-		
+
+		console.log("解析输出")
+		console.log(out)
 		return out
 	},
 
-	getBgContext:function(){
-		return [this.last_tc,this.last_pc]
+	getBgContext: function() {
+		return [this.last_tc, this.last_pc]
 	}
 }
 
